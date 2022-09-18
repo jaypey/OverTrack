090429ea34c8c5876b3ce43d3a4984969b8e701c
Victor Turgeon <masfervic@hotmail.com>
2022-09-18 13:36:32 -0400

    Retrait des methodes inutile (on garde seulement le password qui sera encrypté)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,20 +2,4 @@ class User < ApplicationRecord
   def password=(value)
     super(BCrypt::Password.create(value))
   end
-
-  def email=(value)
-    super(value)
-  end 
-
-  def first_name=(value)
-    super(value)
-  end
-
-  def last_name=(value)
-    super(value)
-  end
-
-  def phone_number=(value)
-    super(value)
-  end
 end
