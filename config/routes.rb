fa53c2efd00c385c82c2018f37426b24104b6bf0
mazacra <mazacra12345@gmail.com>
2022-09-22 14:52:21 -0400

    Modification routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,4 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#not_found"
   get "/500", to: "errors#not_found"
-
-  get "register", to: "sessions#register"
 end
