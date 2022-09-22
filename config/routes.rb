cd984bd6e9c865f56d1b7656c434e148b99a854e
mazacra <mazacra12345@gmail.com>
2022-09-22 13:43:21 -0400

    Ajout Page et route register

--- config/routes.rb
+++ config/routes.rb
@@ -35,4 +35,6 @@ Rails.application.routes.draw do
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#not_found"
   get "/500", to: "errors#not_found"
+
+  get "register", to: "sessions#register"
 end
