544067abcbe147c418a3d5c1f415382fd6fc34bf
Louis Garceau <2076183@etu.cegepsth.qc.ca>
2022-09-22 12:58:40 -0400

    Ajout de validation sur la classe user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+
+  validates :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :phone, format: { with: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/ }, allow_blank: true
+  validates :password, presence: true
+  validates :password, format: { with: /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$/ }
+
   def password=(value)
     super(BCrypt::Password.create(value))
   end
