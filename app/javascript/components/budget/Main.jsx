b86eb1a0081412fc45f2386136f049c9f369600d
Laurent Brochu <2029155@etu.cegepsth.qc.ca>
2022-10-01 15:53:22 -0400

    OT-16: Ajouter bouton selectionner et autres
    -Bouton Selectionner
    -Texte pour qui demande de selectionner un budget
    -Couleur de bouton

--- app/javascript/components/budget/Main.jsx
+++ app/javascript/components/budget/Main.jsx
@@ -4,38 +4,44 @@ import AddBudgetModal from '../budget/AddBudgetModal';
 class Main extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             //Mocking the data 
             budgets: [
-                {id: 1, name: 'Perso', users: [
-                    {id: 1, firstname: 'Laurent', lastname : 'Brochu'},
-                  ]},
-                {id: 2, name: 'Famille',users: [
-                    {id: 2, firstname: 'Laurent', lastname : 'Brochu'},
-                    {id: 3, firstname: 'Maxime', lastname : 'Lefebvre'},
-                    {id: 4, firstname: 'Karl', lastname : 'Mainville'},
-                    {id: 5, firstname: 'Louis', lastname : 'Garceau'}
-                  ]},
-                {id: 3, name: 'Entreprise Planto', users: [
-                    {id: 6, firstname: 'Laurent', lastname : 'Brochu'},
-                    {id: 7, firstname: 'Maxime', lastname : 'Aubin'},
-                    {id: 8, firstname: 'Jean-Philippe', lastname : 'Belval'}
-                  ]}
-              ],
+                {
+                    id: 1, name: 'Perso', users: [
+                        { id: 1, firstname: 'Laurent', lastname: 'Brochu' },
+                    ]
+                },
+                {
+                    id: 2, name: 'Famille', users: [
+                        { id: 2, firstname: 'Laurent', lastname: 'Brochu' },
+                        { id: 3, firstname: 'Maxime', lastname: 'Lefebvre' },
+                        { id: 4, firstname: 'Karl', lastname: 'Mainville' },
+                        { id: 5, firstname: 'Louis', lastname: 'Garceau' }
+                    ]
+                },
+                {
+                    id: 3, name: 'Entreprise Planto', users: [
+                        { id: 6, firstname: 'Laurent', lastname: 'Brochu' },
+                        { id: 7, firstname: 'Maxime', lastname: 'Aubin' },
+                        { id: 8, firstname: 'Jean-Philippe', lastname: 'Belval' }
+                    ]
+                }
+            ],
             idSelectedBudget: 0,
             showBudgetCreate: false,
         };
     }
-    
+
     openBudgetCreate = () => { this.setState({ showBudgetCreate: true }); }
     closeBudgetCreate = () => { this.setState({ showBudgetCreate: false }); }
-    changeSelectedBudget = async (id) => { this.setState({ idSelectedBudget: id })}
+    changeSelectedBudget = async (id) => { this.setState({ idSelectedBudget: id }) }
 
     renderBudgetCreateModal() {
-    if (!this.state.showBudgetCreate) { return ''; }
-    //return <AddBudgetModal onClose={this.closeBudgetCreate} onSave={this.onBudgetSave} />;
-    return <AddBudgetModal onClose={this.closeBudgetCreate} />;
+        if (!this.state.showBudgetCreate) { return ''; }
+        //return <AddBudgetModal onClose={this.closeBudgetCreate} onSave={this.onBudgetSave} />;
+        return <AddBudgetModal onClose={this.closeBudgetCreate} />;
     }
 
 
@@ -64,7 +70,7 @@ class Main extends React.Component {
     }
 
     renderBudgetInfo(budget) {
-        if (this.state.idSelectedBudget === 0) { return ''; }
+        if (this.state.idSelectedBudget === 0) { return 'Select a budget'; }
         return (
             <div className="content">
                 <div className="overflow-x bg-gray p-10">
@@ -81,28 +87,32 @@ class Main extends React.Component {
                         </tbody>
                     </table>
                 </div>
+                <br/>
+                <div>
+                    <button className='btn btn-round btn-accept pos-abs mt-neg-20 z-5'>Select</button>
+                </div>
             </div>
         )
     }
 
-    
+
     renderBudgets() {
-        
+
         return (
             <div className='content-with-sidebar mt-30'>
                 <div className='sidebar'>
                     <h3 className=''>Your budgets</h3>
                     <ul>
                         {this.state.budgets.map((budget) =>
-                        (<li key={budget.id} 
-                        onClick={() => this.changeSelectedBudget(budget.id)} 
-                        style={{ cursor: 'pointer'}} 
-                        className={budget.id == this.state.idSelectedBudget ? 'input-group text-bold' : 'input-group'}
+                        (<li key={budget.id}
+                            onClick={() => this.changeSelectedBudget(budget.id)}
+                            style={{ cursor: 'pointer' }}
+                            className={budget.id == this.state.idSelectedBudget ? 'input-group text-bold' : 'input-group'}
                         >
                             {budget.name}
                         </li>))}
                     </ul>
-                    <br/>
+                    <br />
                     <div>
                         <button className='btn btn-round btn-dark pos-abs mt-neg-20 z-5' onClick={this.openBudgetCreate}>+ Add Budget</button>
                     </div>
@@ -115,7 +125,7 @@ class Main extends React.Component {
     }
 
     render() {
-        return(
+        return (
             <div className='container wide'>
                 {this.renderBudgetCreateModal()}
                 {this.renderBudgets()}
