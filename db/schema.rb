7d3a01b12dc5daa8c36111f6198a6b6214a93f1a
MAubin <mazacra12345@gmail.com>
2022-10-06 14:20:43 -0400

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_22_204705) do
+ActiveRecord::Schema.define(version: 2022_09_30_163357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2022_09_22_204705) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
     t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
