45919d1330d92d401df1ae0c4b47aa5d9b674306
Maxime Lefebvre <2044127@etu.cegepsth.qc.ca>
2022-11-14 13:07:04 -0500

    Fix API Tasks + loadData dans main

--- app/controllers/api/v1/task_controller.rb
+++ app/controllers/api/v1/task_controller.rb
@@ -1,13 +1,13 @@
 module Api; module V1
     class TaskController < ApplicationController
         def index
-            render json: ::Task.where({budgets_id: cookies.signed[:selectedBudget]}).order(:due_date)
+            render json: ::Task.where({budget_id: cookies.signed[:selectedBudget]}).order(:due_date)
             # add .order(:due_date)
         end
 
         def create
             budget = ::Budget.find(cookies.signed[:selectedBudget])
-            task = ::Task.new(is_done: false, title: params[:title], description: params[:description], due_date: params[:due_date], category_id: params[:category_id], budgets_id: budget.id)
+            task = ::Task.new(is_done: false, title: params[:title], description: params[:description], due_date: params[:due_date], category_id: params[:category_id], budget_id: budget.id)
             successful = task.save
             render json: task, status: successful ? 200 : 500
         end
--- app/javascript/components/tasks/Main.jsx
+++ app/javascript/components/tasks/Main.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
 import moment from 'moment';
 import ExpenseFormModal from '../expenses/FormModal';
-import { Categories, Revenues, Expenses, Goals, Reports } from '../../api/main';
+import { Categories, Revenues, Expenses, Goals, Reports, Tasks } from '../../api/main';
 import { Alerts } from '../../helpers/main';
+
+// Besoins (API)
+// Toutes les catégories
+// Toutes les tâches
+
+// Components à créer
+// TasksList
+// TaskTile
+// CategoriesList
+// CategoryTile
+// FormModal (Pour le edit + details)
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      categories: [],
+      tasks: [],
     };
+  }
+
+  componentDidMount() {
+    this.reloadData();
+  }
+
+  reloadData = () => {
+    this.loadCategories();
+    this.loadTasks();
+  }
+
+  loadCategories = () => {
+    Categories.list().then(
+      (cResp) => { this.setState({ categories: cResp }); },
+      () => { Alerts.error("Categories didn't load correctly"); }
+    );
+  }
+
+  loadTasks = () => {
+    Tasks.list().then(
+      (cResp) => { this.setState({ tasks: cResp }); },
+      () => { Alerts.error("Tasks didn't load correctly"); }
+    );
   }
 
   render() {
