0a4e274711a403445239d6a4c59fe70f7e212e66
Maximus477 <maximelefebvre268@gmail.com>
2022-11-24 12:20:10 -0500

    btn sortDone fonctionnel

--- app/javascript/components/tasks/Main.jsx
+++ app/javascript/components/tasks/Main.jsx
@@ -22,6 +22,10 @@ class Main extends React.Component {
     this.state = {
       categories: [],
       tasks: [],
+      doneSortState: true,
+      TitleSortState: true,
+      CategorySortState: true,
+      DateSortState: true,
       showTaskCreateModal: false,
     };
   }
@@ -62,7 +66,39 @@ class Main extends React.Component {
     return <TaskFormModal onClose={this.closeTaskCreate} onSave={this.onTaskSave} categories={this.state.categories}/>;
   }
 
+  toggleDoneSort = () => {
+
+    if(this.state.doneSortState){
+      this.setState({ tasks: this.state.tasks.sort((a, b) =>  a.is_done - b.is_done)});
+      this.state.doneSortState = false;
+    } else {
+      this.setState({ tasks: this.state.tasks.sort((a, b) =>  b.is_done - a.is_done)});
+      this.state.doneSortState = true;
+    }
+
+    // console.log(this.state.tasks);
+    // this.state.tasks.sort((a, b) =>  a.id - b.id);
+    // console.log(this.state.tasks);
+    // this.state.tasks.sort((a, b) =>  b.id - a.id);
+    // console.log(this.state.tasks);
+  }
+  toggleTitleSort = () => {
+    this.state.tasks.sort((a, b) => a.is_done - b.is_done);
+  }
+  toggleCategorySort = () => {
+    this.state.tasks.sort((a, b) => a.is_done - b.is_done);
+  }
+  toggleDateSort = () => {
+    this.state.tasks.sort((a, b) => a.is_done - b.is_done);
+  }
+
   render() {
+    let buttonDone;
+    if (this.state.doneSortState) {
+      buttonDone = <button onClick={this.toggleDoneSort} className='button-sort button-1'>Done <i id="icDone" className="arrow down"></i></button>
+    } else {
+      buttonDone = <button onClick={this.toggleDoneSort} className='button-sort button-1'>Done <i id="icDone" className="arrow up"></i></button>
+    }
     return (
       <div className='container'>
         {this.renderTaskCreateModal()}
@@ -72,10 +108,10 @@ class Main extends React.Component {
           />
         <button className="btn btn-round btn-dark mt-10" onClick={this.openTaskCreate}>+ add a task</button>
         <div style={{marginTop: 40, height: 40}}>
-          <button className='button-sort button-1'>Done <i class="arrow down"></i></button>
-          <button className='button-sort button-2'>Title <i class="arrow down"></i></button>
-          <button className='button-sort button-3'>Category <i class="arrow down"></i></button>
-          <button className='button-sort button-4'>Due date <i class="arrow down"></i></button>
+          {buttonDone}
+          <button onClick={this.toggleTitleSort} className='button-sort button-2'>Title <i id="icTitle" className="arrow down"></i></button>
+          <button onClick={this.toggleCategorySort} className='button-sort button-3'>Category <i id="icCategory" className="arrow down"></i></button>
+          <button onClick={this.toggleDateSort} className='button-sort button-4'>Due date <i id="icDate" className="arrow down"></i></button>
         </div>
         <TasksList tasks={this.state.tasks} categories={this.state.categories} onChange={this.reloadData} />
       </div>
