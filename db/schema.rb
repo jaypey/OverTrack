f92c6a9cd61925d5be6e281a8a8f7e8e30bc95a2
MAubin <mazacra12345@gmail.com>
2022-12-01 13:42:17 -0500

    Shcema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2031_42_15_562252) do
+ActiveRecord::Schema.define(version: 2031_42_15_562254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,8 @@ ActiveRecord::Schema.define(version: 2031_42_15_562252) do
     t.string "password_digest"
     t.string "token"
     t.boolean "confirmed"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "budgets", "users", column: "owner_id"
