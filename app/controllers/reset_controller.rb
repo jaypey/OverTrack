211c717b57882199f9ba5f5a1266cb6956ed62a1
MAubin <mazacra12345@gmail.com>
2022-12-01 13:51:25 -0500

    regex email

--- app/controllers/reset_controller.rb
+++ app/controllers/reset_controller.rb
@@ -7,7 +7,13 @@ class ResetController < ActionController::Base
       redirect_to :controller => 'user', :action => 'forgot'
       return
     end
-    
+
+    if !params[:email].match(/(?:[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+      flash[:error] = 'Email not valid'
+      redirect_to :controller => 'user', :action => 'forgot'
+      return
+    end  
+
     user = User.find_by(email: params[:email]) # if present find user by email
     
     if user.present?
