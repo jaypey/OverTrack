e53c2035144c34bd5ef251af671878e47acb8e79
jaypey <jp@belval.org>
2022-12-04 15:29:10 -0500

    Ajout du HTTPOnly sur les cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2031_42_15_562254) do
+ActiveRecord::Schema.define(version: 2031_42_15_562255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
